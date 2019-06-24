310d124c6a6b68c9b163a8eab8c9cc9bd05ca33f
Eliot Howes <ejhowes@gmail.com>
2019-06-24 15:39:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,11 +5,8 @@ def nyc_pigeon_organizer(data)
   data_by_name = {}
 
   data.each do |attribute1, attribute2|
-# binding.pry
     attribute2.each do |info, names|
-# binding.pry
       names.each do |name|
-# binding.pry
         data_by_name[name] = {:color => [], :gender => [], :lives => []}
       end
     end
@@ -17,23 +14,11 @@ def nyc_pigeon_organizer(data)
   data_by_name.each do |name, attribute|
     data.each_pair do |attribute2, type|
       type.each do |a, b|
-# binding.pry
         if b.include?(name)
           data_by_name[name][attribute2] << a.to_s
         end
       end
     end
   end
-  # data_by_name.each do |p_name, info|
-  #   data.each do |attribute1, attribute2|
-  #     attribute2.each do |info, names|
-  #       names.each_with_index do |name, i|
-  #         if name == p_name
-  #           data_by_name[p_name][:colour][i] = info.to_s
-  #         end
-  #       end
-  #     end
-  #   end
-  # end
   data_by_name
 end
