d2ab994e38e9f7c21683aa0715eacf6b2115227a
Harry Chopra <harrychopra@gmail.com>
2019-07-09 15:55:18 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ f= {}
+ a = []
+ a << data[:color].values
+ b = a.flatten.uniq
+
+ b.each { |name| f["#{name}"] = {:color => [], :gender => [], :lives =>[]} }
+ b.each{ |bname|
+  data.each{ |label, list|
+    list.each{ |info, names|
+      if names.include?(bname)
+        f[bname][label] << info.to_s
+      end
+     }
+   }
+  }
+f
 end
